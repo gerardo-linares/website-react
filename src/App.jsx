48a5535b7b624554a590a6b9b688529ad43bb6cc
gerardo linares <geracoder@gmail.com>
2023-07-05 18:31:20 -0300

    Add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./modules/Home";
@@ -13,22 +14,39 @@ import Certificado from "./modules/Certificado";
 import Franquicia from "./modules/Franquicia";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay for demonstration purposes
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/valoracion" element={<Valoration />} />
-        <Route path="/nudapropiedad" element={<NudaProp />} />
-        <Route path="/digitalizacion" element={<Digital />} />
-        <Route path="/Seguro" element={<Seguro />} />
-        <Route path="/franquicia" element={<Franquicia />} />
-        <Route path="/cartera" element={<Cartera />} />
-        <Route path="/certificado" element={<Certificado />} />
-        <Route path="/vpo" element={<Vpo />} />
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
-      <Footer />
+      {isLoading ? (
+        <div className="text-center text-4xl text-primary mt-80 font-slogan">
+          Cargando el sitio...
+        </div>
+      ) : (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/valoracion" element={<Valoration />} />
+            <Route path="/nudapropiedad" element={<NudaProp />} />
+            <Route path="/digitalizacion" element={<Digital />} />
+            <Route path="/Seguro" element={<Seguro />} />
+            <Route path="/franquicia" element={<Franquicia />} />
+            <Route path="/cartera" element={<Cartera />} />
+            <Route path="/certificado" element={<Certificado />} />
+            <Route path="/vpo" element={<Vpo />} />
+            <Route path="*" element={<div>404</div>} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
